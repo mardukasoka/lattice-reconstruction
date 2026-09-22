872799863fa7b7dedc9fbd14c19d419fcf0f357e
Bel <zephyr9673@gmail.com>
2026-09-23 01:15:31 +0930

    Add reusable LRA catalogue renderer

---
+++ web/play/lra-catalogue-ui.js
@@ -0,0 +1,10 @@
+(function(){
+  var cat=window.LRA_CATALOGUE, host=document.getElementById("lra-catalogue");
+  if(!cat||!host)return;
+  var esc=function(s){return String(s).replace(/[&<>"']/g,function(c){return({"&":"&amp;","<":"&lt;",">":"&gt;",'"':"&quot;","'":"&#39;"})[c]})};
+  var out="<summary>"+esc(cat.centre.toUpperCase())+" · OPEN OBJECTS</summary>";
+  cat.objects.forEach(function(o){
+    out+='<div class="lra-object"><img src="'+esc(o.image)+'" alt="'+esc(o.title+" "+o.id)+'"><div><strong>'+esc(o.id)+" · "+esc(o.title)+'</strong><div class="lra-meta">'+esc(o.meta)+'<br>Image: '+esc(o.attribution)+'</div><div class="lra-links"><a href="'+esc(o.imagePage)+'" target="_blank" rel="noopener">IMAGE</a><a href="'+esc(o.source)+'" target="_blank" rel="noopener">SOURCE</a></div></div></div>';
+  });
+  host.innerHTML=out;
+})();
